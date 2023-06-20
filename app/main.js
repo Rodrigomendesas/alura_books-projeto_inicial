944f49b704e49a644bde78d7acb52416597fa30b
Rodrigo MA Silva <99435093+Rodrigomendesas@users.noreply.github.com>
2023-06-20 19:27:19 -0300

    Uploading the project files

---
+++ app/main.js
@@ -0,0 +1,13 @@
+let livros = [];
+const endpointDaAPI = 'https://guilhermeonrails.github.io/casadocodigo/livros.json';
+getBuscarLivrosDaAPI();
+const elementoParaInserirLivros = document.getElementById('livros')
+const elementoValorTotalLivrosDisponiveis = document.getElementById("valor_total_livros_disponiveis");
+
+async function getBuscarLivrosDaAPI(){
+    const res = await fetch(endpointDaAPI);
+    livros = await res.json();
+    let livrosComDesconto = aplicarDesconto(livros);
+    exibirLivrosNaTela(livrosComDesconto);
+}
+
